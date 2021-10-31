21cdffcc6aaf672eb55d07c9ccb9a1e10fd67bbc
Eyüp Yıldırım <eyup.yildirim02@gmail.com>
2021-10-31 04:33:15 +0300

    basic social links added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
-import './App.css';
-
+import './App.css'
+import FacebookIcon from './images/social-icons/facebook.png'
+import GithubIcon from './images/social-icons/github.png'
+import InstagramIcon from './images/social-icons/instagram.png'
+import MediumIcon from './images/social-icons/medium.png'
+import TwitterIcon from './images/social-icons/twitter.png'
+import YoutubeIcon from './images/social-icons/youtube.png'
+import LinkedinIcon from './images/social-icons/linkedin.png'
+import ProfilePhoto from './images/eymel.jpeg'
+import CircleUser from './components/circle-user'
+const socialLinks = [
+  {
+    url: 'https://github.com/eymel',
+    icon: GithubIcon
+  },
+  {
+    url: 'https://twitter.com/eeymel',
+    icon: TwitterIcon
+  },
+  {
+    url: 'https://www.youtube.com/channel/UCFRFQbsLRueMLh0Hg4y-wcw',
+    icon: YoutubeIcon
+  },
+  {
+    url: 'https://www.instagram.com/eyymel/',
+    icon: InstagramIcon
+  },
+  {
+    url: 'https://www.linkedin.com/in/eyup-yildirim/',
+    icon: LinkedinIcon
+  }
+]
 function App() {
   return (
     <div className="App">
-      <h1>Eyüp Yıldırım</h1>
-      <h1>eyup.me</h1>
-      
+      <CircleUser profilePhoto={ProfilePhoto} socialLinks={socialLinks} />
+
+      <div styl>
+        <h1>Eyüp Yıldırım</h1>
+        <h1>eyup.me</h1>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/circle-user.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react'
+import '../App.css'
+function CircleUser(params) {
+  return (
+    <div class="circle-container">
+      <img class="center profile-photo" src={params.profilePhoto} />
+
+      {params.socialLinks.map((item, index) => {
+        var rotateAvg = (360 / params.socialLinks.length) * index
+        return (
+          <a
+            href={item.url}
+            class="icon "
+            style={{
+              transform:
+                'rotate(' +
+                rotateAvg +
+                'deg) translate(12em) rotate(-' +
+                rotateAvg +
+                'deg)'
+             
+            }}
+          >
+            <img src={item.icon} />
+          </a>
+        )
+      })}
+    </div>
+  )
+}
+export default CircleUser
